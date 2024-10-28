f66c8f93db7a29b620610f724d9ef0ceb9794f3e
AurelienNioche <nioche.aurelien@gmail.com>
2024-10-28 19:04:19 +0200

    add hydra

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
+import warnings
 
+import hydra
 import pandas as pd
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import mlflow
-
 from mlflow.data.pandas_dataset import from_pandas
+from omegaconf import DictConfig, OmegaConf
+
+import logging
+logging.getLogger("mlflow").setLevel(logging.ERROR)
 
 
 class Model(torch.nn.Module):
@@ -19,49 +24,37 @@ class Model(torch.nn.Module):
         return self.layer(x)
 
 
-def main():
+@hydra.main(config_path="conf", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    warnings.filterwarnings("ignore", category=UserWarning, module="mlflow")
 
-    experiment_name = "my favorite experiment"
-
-    run_name = "my favorite model"
-    run_tags = {"tag1": "value1", "tag2": "value2"}
-    run_description = "This is a wonderful model trained on XYZ data."
-    run_params = {"param1": 1, "param2": 2}
+    print(OmegaConf.to_yaml(cfg, resolve=True))
 
     model_artifact_path = "my_fancy_model"
     model_reg_name = "my_fancy_model_for_registration"
 
     metadata_artifact_path = "metadata"
-    metadata_local_folder_path = "metadata"
+    metadata_local_folder_path = "data"
     metadata_file_name = "metadata.json"
 
     metadata_name = "metadata"
 
+    model_uri = os.getenv("MLFLOW_TRACKING_URI")
+
     load_dotenv(override=True)
     model = Model()
 
-    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
-    mlflow.set_experiment(experiment_name)
+    mlflow.set_tracking_uri(model_uri)
+    mlflow.set_experiment(cfg.mlflow.experiment_name)
 
     metadata_local_file = f"{metadata_local_folder_path}/{metadata_file_name}"
 
-    # Model registration ----------------------------------------------------
-    # Model level tags and description --------------------------------------
-    model_reg_tags = {
-        "model_characteristic": "value1"
-    }
-    model_reg_description = "This is a wonderful model trained on XYZ data."
-    # Version specific tags and description ---------------------------------
-    version_specific_reg_tags = {
-        "tag1": "value1",
-        "tag2": "value2"
-    }
-    version_specific_reg_alias = "alias1"
-    version_specific_reg_description = "This is the description for version 1"
+    with mlflow.start_run(
+            run_name=cfg.mlflow.run_name,
+            description=cfg.mlflow.run_description) as run:
 
-    with mlflow.start_run(run_name=run_name, description=run_description) as run:
-
-        mlflow.log_params(run_params)
+        # Log params from config
+        mlflow.log_params(dict(**cfg.flavor))
 
         # Run and dynamic log of metrics ---------------------------------------------------------------------
         for epoch in range(0, 3):
@@ -76,7 +69,7 @@ def main():
             "recall": np.random.random(),
         }
         mlflow.log_table(data=metrics, artifact_file="tables/metrics.json")
-        mlflow.set_tags(run_tags)
+        mlflow.set_tags(cfg.mlflow.run_tags)
 
         # Log the model ------------------------------------------------------------------------------
         model_info = mlflow.pytorch.log_model(
@@ -84,7 +77,7 @@ def main():
 
         # Log metadata --------------------------------------------------------------------------------
         mlflow.log_artifact(metadata_local_file, artifact_path=metadata_artifact_path)
-        experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
+        experiment_id = mlflow.get_experiment_by_name(cfg.mlflow.experiment_name).experiment_id
         run_id = run.info.run_id
         metadata = pd.read_csv(metadata_local_file)
         source = f"{os.getenv('MLFLOW_TRACKING_URI')}/#/experiments/{experiment_id}/runs/{run_id}/artifacts/{metadata_artifact_path}/{metadata_local_file}"
@@ -96,33 +89,41 @@ def main():
         ax.plot(range(10), np.random.random(10))
         mlflow.log_figure(fig, "figures/whatever_time_series.png")
 
-        # Register the model -------------------------------------------------------------------------
-
-        latest_version = mlflow.register_model(
-            model_uri=model_info.model_uri,
-            name=model_reg_name,
-            tags=version_specific_reg_tags)
-
-        # Client for updating the registered model -------------------------------------------------
+        # Client for the registered model -------------------------------------------------
         client = mlflow.tracking.MlflowClient()
 
-        # Version specific stuff ---------------------
+        # Register the model -------------------------------------------------------------------------
+        models = client.search_registered_models(f"name='{cfg.mlflow.model_name}'")
 
+        if not models:
+            # Register the model if it doesn't exist
+            new_version = mlflow.register_model(
+                model_uri=model_info.model_uri,
+                name=cfg.mlflow.model_name,
+                tags=cfg.mlflow.model_tags,)
+        else:
+            # Explicitly create a new version
+            new_version = client.create_model_version(
+                name=cfg.mlflow.model_name,
+                source=model_info.model_uri,
+                run_id=run.info.run_id
+            )
+        # Version specific stuff ---------------------
         client.set_registered_model_alias(
-            name=model_reg_name,
-            alias=version_specific_reg_alias,
-            version=latest_version.version)
+            name=cfg.mlflow.model_name,
+            alias=cfg.mlflow.model_version_alias,
+            version=new_version.version)
         client.update_model_version(
-            name=model_reg_name,
-            version=latest_version.version,
-            description=version_specific_reg_description
+            name=cfg.mlflow.model_name,
+            version=new_version.version,
+            description=cfg.mlflow.model_version_description
         )
         # High level description/tags for the registered model shared across all versions ----------------
         client.update_registered_model(
-            name=model_reg_name,
-            description=model_reg_description
+            name=cfg.mlflow.model_name,
+            description=cfg.mlflow.model_description,
         )
-        for k, val in model_reg_tags.items():
+        for k, val in cfg.mlflow.model_tags.items():
             client.set_registered_model_tag(model_reg_name, k, val)
 
     # print(mlflow.MlflowClient().get_run(run.info.run_id).data)
